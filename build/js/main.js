f72ae7b43295dcefcc5015bdfdf0e506bba7ce95
Axel Nilsson <axel.nilsson@plakat.se>
2015-10-20 14:59:59 +0200

    testing

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,10 @@
 import * as scripts from './script';
 import { one, two, plus } from './moreScript';
+import $ from 'jquery';
 
 scripts.a();
 plus(one, two);
+
+$(() => {
+    console.log('jQuery is loaded');
+});
