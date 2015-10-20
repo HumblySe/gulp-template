2ca291e084313d81d29f493d169b2016eb167a7d
Janne Mårtensgård <janne.martensgard@plakat.se>
2015-10-20 09:10:40 +0200

    Changed lots of things around

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,65 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var env = require('./environment.json');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
-var refresh = require('gulp-livereload');
-var lr = require('tiny-lr');
-var server = lr();
-var minifyCSS = require('gulp-minify-css');
-var embedlr = require('gulp-embedlr');
+var minifycss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var browsersync = require('browser-sync').create();
 
-gulp.task('scripts', function() {
-    gulp.src(['build/js/**/*.js'])
-        .pipe(browserify())
-        .pipe(concat('script.js'))
-        .pipe(gulp.dest('js/'))
-        .pipe(refresh(server))
-})
+// dev tasks
+gulp.task('dev:css', function(cb) {
+	gulp.src('./build/less/style.less')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(browsersync.stream());
+});
 
-gulp.task('styles', function() {
-    gulp.src(['build/less/style.less'])
-        .pipe(less())
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('css'))
-        .pipe(refresh(server))
-})
+gulp.task('dev:watch', function() {
+	gulp.watch('build/less/style.less', function() { gulp.run('dev:css'); });
+});
 
-gulp.task('lr-server', function() {
-    server.listen(35729, function(err) {
-        if(err) return console.log(err);
-    });
-})
+gulp.task('dev:browsersync', function() {
+	console.log(env.proxy);
+	browsersync.init({
+		proxy: env.proxy,
+		port: 9999
+	});
+});
 
-gulp.task('html', function() {
-    gulp.src("build/html/*.html")
-        .pipe(embedlr())
-        .pipe(gulp.dest(''))
-        .pipe(refresh(server));
-})
+// vendor tasks
+gulp.task('vendor:css', function(cb) {
+	gulp.src('./build/less/vendor.less')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./dist/css'))
+});
 
-gulp.task('default', ['lr-server', 'scripts', 'styles', 'html'], function() {
-    
+// dist tasks
+gulp.task('dist:vendor-css', function(cb) {
+	gulp.src('./build/less/vendor.less')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(minifycss())
+		.pipe(rename('vendor.min.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./dist/css'))
+});
+gulp.task('dist:css', function(cb) {
+	gulp.src('./build/less/style.css')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(minifycss())
+		.pipe(rename('style.min.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./dist/css'))
+});
 
-    gulp.watch('build/js/**', function(event) {
-        gulp.run('scripts');
-    })
+gulp.task('dev', ['dev:css', 'dev:browsersync', 'dev:watch']);
 
-    gulp.watch('build/less/**', function(event) {
-        gulp.run('styles');
-    })
+gulp.task('vendor', ['vendor:css']);
 
-    gulp.watch('app/**/*.html', function(event) {
-        gulp.run('html');
-    })
-})
+gulp.task('dist', ['dist:vendor-css','dist:css']);
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-Hi, initial commit done!
+# Gulp-template
+
+This is the new and improved gulpfile adjusted for use in assorted projects.
+Local environment variables are set in the file **environment.json**, which by default is gitignored. An example configuration file is included as environment-default.json, remember to adjust and rename accordingly.
+
+Browsersync is included. It is by default set to proxy a server at localhost:9999, change this to match your local dev environment in environment.json.
+
+This is a work in progress.
